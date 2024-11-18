c2ce7b0a34308d76014e22b81da1784bc2e4c123
Divyanshi Oberoi <contactdiv01@gmail.com>
2024-11-18 14:16:28 -0600

    added all frontend till now

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,18 +1,21 @@
 import { assets, blog_data } from '@/Assets/assets'
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
-const BlogItem = () => {
+const BlogItem = ({title,description,category,image,id}) => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-      <Image src={blog_data[0].image} alt="" width={400} height={400} className="border border-black" />
-      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{blog_data[0].category}</p>
+      <Link href={`/blogs/${id}`}>
+      <Image src={image} alt="" width={400} height={400} className="border border-black" />
+      </Link>
+      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
       <div className="p5">
-        <h5 className='mb-2 text-lg tracking-tight text-gray-900'>{blog_data[0].title}</h5>
-        <p className='mb-3 text-sm tracking-tight text-gray-700'>{blog_data[0].description}</p>
-        <div className='inline-flex items-center py-2 font-semibold text-center'>
+        <h5 className='mb-2 text-lg tracking-tight text-gray-900'>{title}</h5>
+        <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
+        <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
           Read more <Image src={assets.arrow} className='ml-2' alt='' />
-        </div>
+        </Link>
       </div>
     </div>
   )
